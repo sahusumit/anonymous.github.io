f839e6f3b8ae086d71d841666ca8fa4340164fb4
sksahu <sahusumit999@gmail.com>
2018-03-21 17:19:52 +0000

    first commit

---
+++ app/models/post.rb
@@ -0,0 +1,6 @@
+class Post < ActiveRecord::Base
+    
+    has_attached_file :image, styles: { larger: "600x600>", medium: "300x300>", thumb: "100x100>" }
+     validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+    
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180316134415) do
+ActiveRecord::Schema.define(version: 20180321150425) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "post_id"
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
+  end
 
   create_table "videos", force: :cascade do |t|
     t.string   "wistia"
